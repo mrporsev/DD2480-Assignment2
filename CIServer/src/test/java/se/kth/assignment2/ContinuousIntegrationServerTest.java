3f0b5a732a7da6a9d9c4c9dfe5389da1bf8de3fb
David Cederström <davidced@kth.se>
2023-02-08 12:10:26 +0100

    test(CI-server): Wrote a skeleton for which tests that could be interesting to run #2

--- CIServer/src/test/java/se/kth/assignment2/ContinuousIntegrationServerTest.java
+++ CIServer/src/test/java/se/kth/assignment2/ContinuousIntegrationServerTest.java
@@ -2,6 +2,82 @@ package se.kth.assignment2;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
+
 class ContinuousIntegrationServerTest {
 
+    @Test
+    void correctBranch() {
+
+    }
+
+    @Test
+    void icorrectBranch() {
+
+    }
+
+    @Test
+    void correctRepositoryURL() {
+
+    }
+
+    @Test
+    void icorrectRepositoryURL() {
+
+    }
+
+    @Test
+    void correctRepositoryName() {
+
+    }
+
+    @Test
+    void icorrectRepositoryName() {
+
+    }
+
+    @Test
+    void correctCommitHash() {
+
+    }
+
+    @Test
+    void icorrectCommitHash() {
+
+    }
+
+    @Test
+    void correctCloneURL() {
+
+    }
+
+    @Test
+    void icorrectCloneURL() {
+
+    }
+
+    @Test
+    void handlerValidInput() {
+
+    }
+
+    @Test
+    void handlerInvalidInput() {
+
+    }
+
+    @Test
+    void statushandlerValidInput() {
+
+    }
+
+    @Test
+    void statushandlerIvalidInput() {
+
+    }
+
+    @Test
+    void validJasonObject() {
+
+    }
 }
