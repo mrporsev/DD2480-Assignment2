fa125e306f8d3f07a5e40792887f1c74e01b18e4
pontusnorman123 <pontusnorman1@gmail.com>
2023-02-08 23:16:26 +0100

    test(RequestHandler): test getJason() #51

--- CIServer/src/test/java/se/kth/assignment2/RequestHandlerTest.java
+++ CIServer/src/test/java/se/kth/assignment2/RequestHandlerTest.java
@@ -1,5 +1,6 @@
 package se.kth.assignment2;
 
+import org.json.JSONObject;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,8 +10,32 @@ import static org.junit.jupiter.api.Assertions.*;
 //test class
 class RequestHandlerTest {
 
+    private JSONObject json;
+    private JSONObject pull_json;
+    private JSONObject head_json;
+    private JSONObject repo_json;
+    private JSONObject clone_json;
+
+
+
+
+    RequestHandler rh;
     @BeforeEach
     void setUp() {
+        json = new JSONObject();
+        pull_json = new JSONObject();
+        head_json = new JSONObject();
+        clone_json = new JSONObject();
+
+        clone_json.put("clone_url","my_url");
+        head_json.put("ref","my_ref");
+        head_json.put("sha","my_sha");
+        pull_json.put("head",head_json);
+        json.put("pull_request",pull_json);
+        json.put("repository",clone_json);
+
+        rh = new RequestHandler(json);
+
     }
 
     @AfterEach
@@ -19,6 +44,8 @@ class RequestHandlerTest {
 
     @Test
     void getJsonObject() {
+
+        assertTrue(rh.getJsonObject() == json);
     }
 
     @Test
