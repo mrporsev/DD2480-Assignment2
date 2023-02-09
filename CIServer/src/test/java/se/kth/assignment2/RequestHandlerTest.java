3500edb41c4461d0803f1fec1d7d7a3ad0af4c04
David Cederström <davidced@kth.se>
2023-02-09 12:39:15 +0100

    test(TriggerTest): Commit to trigger the build #5

--- CIServer/src/test/java/se/kth/assignment2/RequestHandlerTest.java
+++ CIServer/src/test/java/se/kth/assignment2/RequestHandlerTest.java
@@ -70,7 +70,7 @@ class RequestHandlerTest {
         json.put("repository", clone_json);
 
         RequestHandler requestHandler = new RequestHandler(json);
-        String expectedBranch = "test-branch-#1";
+        String expectedBranch = "test-branch-#";
         String actualBranch = requestHandler.getBranch();
         try {
             assertEquals(expectedBranch, actualBranch);
